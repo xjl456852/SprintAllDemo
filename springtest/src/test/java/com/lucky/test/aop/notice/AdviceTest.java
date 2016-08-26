34239c05eb8fdce1e9b6768a3760920769fff6c0
LUCKY <LUCKY@ZHOU>
2016-08-26 14:03:32 +0800

    add aop arguments sources

--- springtest/src/test/java/com/lucky/test/aop/notice/AdviceTest.java
+++ springtest/src/test/java/com/lucky/test/aop/notice/AdviceTest.java
@@ -1,5 +1,7 @@
 package com.lucky.test.aop.notice;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /*
@@ -7,8 +9,37 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class AdviceTest {
 
-	public void adviceTest() {
+	public void adviceTest(JoinPoint joinPoint) {
+		System.out.println("参数一共有" + joinPoint.getArgs().length + "个");
+		int i = 0;
+		for (Object object : joinPoint.getArgs()) {
+
+			System.out.println("第" + i + "个参数是:" + joinPoint.getArgs()[i]);
+			i++;
+		}
 		System.out.println("我是aop，我进去了");
+	}
+
+	public Object adviceTest2(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		try {
+			System.out.println(joinPoint.getArgs().toString());
+			System.out.println(joinPoint.getClass().getName());
+			System.out.println(joinPoint.getKind().toString());
+			System.out.println(joinPoint.getSignature().getDeclaringTypeName()
+					.toString());
+			System.out.println(joinPoint.getSourceLocation().toString());
+			System.out.println("调用的方法是：" + joinPoint.getSignature().getName());
+			System.out.println("调用的类是："
+					+ joinPoint.getTarget().getClass().getName());
+			System.out.println("日志开始时间");
+			return joinPoint.proceed(joinPoint.getArgs());
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			System.out.println("日志结束时间");
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -16,6 +47,6 @@ public class AdviceTest {
 				"test-aop.xml");
 		ctx.start();
 		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
-		helloWorld.sayHello();
+		helloWorld.sayHello("你好吗");
 	}
 }
--- springtest/src/test/java/com/lucky/test/aop/notice/HelloWorld.java
+++ springtest/src/test/java/com/lucky/test/aop/notice/HelloWorld.java
@@ -2,7 +2,7 @@ package com.lucky.test.aop.notice;
 
 public class HelloWorld {
 
-	public void sayHello() {
-		System.out.println("开口说话");
+	public void sayHello(String words) {
+		System.out.println("开口说话"+words);
 	}
 }
